fdf00795ffc5ea7f302dd1b22c59695cd7af7b9d
HI821-art <dimaverem09@gmail.com>
2025-05-10 14:21:26 +0300

    N-lay

---
+++ Core/Helpers/MailJetSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Helpers
+{
+    public class MailJetSettings
+    {
+        public string ApiKey { get; set; }
+        public string SecretKey { get; set; }
+        public string SenderEmail { get; set; }
+        public string SenderName { get; set; }
+    }
+}
---
+++ Core/Interfaces/IFavoriteService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Data.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IFavoriteService
+    {
+        List<int> GetIds();
+        List<Movie> GetAll();
+        void Add(int id);
+        void Remove(int id);
+        void Clear();
+        int GetCount();
+    }
+}
